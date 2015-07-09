110f16f642474fb6be38f4a4f2a19fab495f89c0
Liang-Heng Chen <xeonchen@mozilla.com>
2015-07-09 23:10:03 +0800

    customize shortcut support

--- data/dxr.js
+++ data/dxr.js
@@ -1,9 +1,37 @@
 'use strict';
 
-document.addEventListener('keyup', function(event) {
-  if (event.keyCode == KeyEvent.DOM_VK_SLASH && event.ctrlKey) {
+(function(){
+  var useAlt, useCtrl, useMeta, useShift, keyCode;
+
+  function setFocus() {
     let query = document.getElementById('query');
     query.focus();
     query.select();
   }
-});
+
+  self.port.on('prefs', function(prefs) {
+    console.log(prefs);
+
+    useAlt = prefs.useAlt;
+    useCtrl = prefs.useCtrl;
+    useMeta = prefs.useMeta;
+    useShift = prefs.useShift;
+    keyCode = prefs.keyCode;
+  });
+
+  document.addEventListener('keyup', function(event) {
+    console.log('keyCode = ' + event.keyCode);
+    console.log('altKey = ' + event.altKey);
+    console.log('ctrlKey = ' + event.ctrlKey);
+    console.log('metaKey = ' + event.metaKey);
+    console.log('shiftKey = ' + event.shiftKey);
+
+    if (event.keyCode == keyCode &&
+        event.altKey == useAlt &&
+        event.ctrlKey == useCtrl &&
+        event.metaKey == useMeta &&
+        event.shiftKey == useShift) {
+      setFocus();
+    }
+  });
+})();
